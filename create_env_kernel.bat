f4c87a9c13fd5c6e288866f84e74dce910e21458
mita765 <62315458+mita765@users.noreply.github.com>
2024-08-02 18:37:42 -0500

    Fixed version control

---
+++ create_env_kernel.bat
@@ -0,0 +1,3 @@
+conda init
+conda env create -f thesis.yml
+python -m ipykernel install --user --name=thesis
---
+++ create_env_kernel.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+conda init
+conda env create -f thesis.yml
+python -m ipykernel install --user --name=thesis
